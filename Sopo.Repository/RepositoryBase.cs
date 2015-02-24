a30916e23ef9d0806657ea348cc2476878270802
BurnOutDev <iraklimurusidze@live.com>
2015-02-25 03:57:27 +0400

    Added Delete by IsDeleted Property

--- Sopo.Repository/RepositoryBase.cs
+++ Sopo.Repository/RepositoryBase.cs
@@ -5,51 +5,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sopo.Repository {
+namespace Sopo.Repository
+{
 
-	public abstract class RepositoryBase<T> where T : class, new() {
+    public abstract class RepositoryBase<T> where T : class, new()
+    {
 
-		protected SopoContext _context;
+        protected SopoContext _context;
 
-		public SopoContext Context {
-			get { return _context; }
-		}
+        public SopoContext Context
+        {
+            get { return _context; }
+        }
 
-		internal RepositoryBase(SopoContext context = null) {
-			if(context == null) context = new SopoContext();
+        internal RepositoryBase(SopoContext context = null)
+        {
+            if (context == null) context = new SopoContext();
 
-			_context = context;
-		}
+            _context = context;
+        }
 
-		public virtual T Fetch(int id) {
-			return _context.Set<T>().Find(id);
-		}
+        public virtual T Fetch(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
 
-		public virtual IEnumerable<T> Set() {
-			return _context.Set<T>();
-		}
+        public virtual IEnumerable<T> Set()
+        {
+            return _context.Set<T>();
+        }
 
-		public virtual void Save(T entity) {
-			Save(_context.Set<T>(), entity);
-		}
+        public virtual void Save(T entity)
+        {
+            Save(_context.Set<T>(), entity);
+        }
 
-		protected virtual void Save(DbSet<T> set, T entity) {
-			var entry = _context.Entry(entity);
-			if(entry == null || entry.State == EntityState.Detached) set.Add(entity);
-		}
+        protected virtual void Save(DbSet<T> set, T entity)
+        {
+            var entry = _context.Entry(entity);
+            if (entry == null || entry.State == EntityState.Detached) set.Add(entity);
+        }
 
-		public virtual void Delete(int id) {
-			Delete(Fetch(id));
-		}
+        public virtual void Delete(int id)
+        {
+            Delete(Fetch(id));
+        }
 
-		public virtual void Delete(T entity) {
-			Delete(_context.Set<T>(), entity);
-		}
+        public virtual void Delete(T entity)
+        {
+            Delete(_context.Set<T>(), entity);
+        }
 
-		protected virtual void Delete(DbSet<T> set, T entity) {
-			//movifiqropt meqanizmi romelic mixvdeba aqvs tu ara obieqts romelic ishleba IsDeleted veli,
-			//tu eseti veli gaachnia mashin gadaiyvanos es veli true-ze, tuarada washalof fizikurad.
-			set.Remove(entity);
-		}
-	}
+        protected virtual void Delete(DbSet<T> set, T entity)
+        {
+            //movifiqropt meqanizmi romelic mixvdeba aqvs tu ara obieqts romelic ishleba IsDeleted veli,
+            //tu eseti veli gaachnia mashin gadaiyvanos es veli true-ze, tuarada washalos fizikurad.
+
+            dynamic entityDynamic = entity;
+
+            try { entityDynamic.IsDeleted = true; }
+            catch { set.Remove(entity); }
+        }
+    }
 }
