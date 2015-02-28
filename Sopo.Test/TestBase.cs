753b273e290102f3e16f58897963cf56285e5ebd
BurnOutDev <iraklimurusidze@live.com>
2015-02-28 14:39:33 +0400

    Added UnitTests by David

---
+++ Sopo.Test/TestBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sopo.Repository;
+
+namespace Sopo.Test {
+	public class TestBase {
+		protected SopoContext _context;
+
+		protected TestBase() {
+			_context = new SopoContext();
+		}
+	}
+}
---
+++ Sopo.Test/UserRepositoryTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sopo.Repository;
+using Sopo.Domain;
+
+namespace Sopo.Test {
+	[TestClass]
+	public class UserRepositoryTest : TestBase {
+		private UserRepository _userRepository;
+
+		public UserRepositoryTest() {
+			_userRepository = new UserRepository();
+		}
+
+		[TestMethod]
+		public void ReadWriteTest() {
+			User u = new User() {
+				Email = "testuser@tests.com",
+				Password = "TestPass",
+				FirstName = "TestFirstName",
+				LastName = "TestLastName",
+			};
+
+			_context.Users.Add(u);
+			_context.SaveChanges();
+
+			Assert.IsNotNull(_context.Users.FirstOrDefault(x => x.Email == "testuser@tests.com"), "User wasn't created!");
+
+		}
+	}
+}
